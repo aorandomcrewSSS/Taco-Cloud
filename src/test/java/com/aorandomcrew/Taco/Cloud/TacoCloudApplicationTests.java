ef43a7b9335b4394df122747bbb317b8d8d9b02f
aorandomcrewSSS <danbogomolovhr@gmai.com>
2024-11-16 19:17:48 +0300

    Initial commit

---
+++ src/test/java/com/aorandomcrew/Taco/Cloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aorandomcrew.Taco.Cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
