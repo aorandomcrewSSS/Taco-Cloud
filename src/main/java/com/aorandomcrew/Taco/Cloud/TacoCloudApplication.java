6c7c2b5a5a3e2fa6406ea6e82a6c5035f51f691b
aorandomcrewSSS <danbogomolovhr@gmai.com>
2024-11-25 02:28:46 +0300

    Глава 2. Разработка веб-приложений

--- src/main/java/com/aorandomcrew/Taco/Cloud/TacoCloudApplication.java
+++ src/main/java/com/aorandomcrew/Taco/Cloud/TacoCloudApplication.java
@@ -1,13 +1,19 @@
 package com.aorandomcrew.Taco.Cloud;
 
+import com.aorandomcrew.Taco.Cloud.config.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TacoCloudApplication {
+public class TacoCloudApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
 
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home"); }
 }
